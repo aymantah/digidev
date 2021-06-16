ed4c52bfbe7559e375e95a7f5c49d35fe9033147
aymantah <57458298+aymantah@users.noreply.github.com>
2021-06-16 22:32:45 +0100

    changess

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/ayman.jpg" alt="" />
+          <img src="assets/men1.png" alt="" />
         </div>
       </div>
       <div className="right">
